a65f2759dc31a93cb4cd2b8cb384b6629036ad54
Paulo Watakabe <watakabe05@gmail.com>
2015-05-19 15:41:03 -0300

    first commit

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -18,4 +18,9 @@ class IndexController extends AbstractActionController
     {
         return new ViewModel();
     }
+    
+    public function logedAction()
+    {
+        return new ViewModel();
+    }
 }
