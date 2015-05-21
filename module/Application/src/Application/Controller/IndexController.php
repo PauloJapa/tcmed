7eb7b2269517e9e042c16c2fb9da1102f0dc7ba7
Paulo Watakabe <watakabe05@gmail.com>
2015-05-21 12:22:06 -0300

    Rotas e teste na action cadastro

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
 
 namespace Application\Controller;
 
@@ -14,13 +7,22 @@ use Zend\View\Model\ViewModel;
 
 class IndexController extends AbstractActionController
 {
+
     public function indexAction()
     {
         return new ViewModel();
     }
-    
+
     public function logedAction()
     {
         return new ViewModel();
     }
+
+    public function cadastroAction()
+    {
+        return new ViewModel();
+    }
+
+
 }
+
