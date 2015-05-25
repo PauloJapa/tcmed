2b0ddeb17f0288d5bc43d3abbc0557a190ef453f
Danilo Dorotheu <danilo.dorotheu@live.com>
2015-05-25 12:56:54 -0300

    Modificação de funções do motor JS: - initAjax; - processa; - lockClick; - unlockClick;

--- public/js/myscript.js
+++ public/js/myscript.js
@@ -1,91 +1,129 @@
-var GLOBALSIS = [];
-var canClick = true;
-
+var GLOBALSIS = {
+    path: "app/",
+    canClick: true,
+    timeOutButton: 30000,
+};
 
 $(function () {
+    $(document).on("click", "#bot", function () {
+        alert(getInputsForm("#frm"));
+    })
 });
 
+
+/**
+ * Timeout de funcoes (delay)
+ * @param {type} time
+ * @param {type} funcao
+ * @returns {initTimeOut.timeOut}
+ */
 function initTimeOut(time, funcao) {
     var timeOut = setTimeout(funcao, time);
     return timeOut;
 }
 
-function getInputsForm(idDoForm) {
-    var dadosDoForm = {};
-    var form = $('#' + idDoForm);
+/**
+ * Transforma os inputs/selects do form em json 
+ * @param {type} idDoForm
+ * @returns {jQuery}
+ */
+getInputsForm = function (obj) {
+    var o = {};
 
-    form.find('input').each(function () {
-        if ($(this).attr('type') == 'checkbox' || $(this).attr('type') == 'radio') {
-            if ($(this).is(':checked')) {
-                dadosDoForm[$(this).attr('name')] = $(this).val();
-            }
-        } else {
-            dadosDoForm[$(this).attr('name')] = $(this).val();
-        }
+    var a = $(obj).serializeArray();
+
+    $.each(a, function () {
+        o[this.name] = this.value || '';
     });
+    return JSON.stringify(o);
+};
 
-    /*
-     form.find('select').find('option:selected').each(function () {
-     dadosDoForm["'" + this.attr('name') + "'"] = this.val();
-     });
-     */
-    return dadosDoForm;
-}
-
-function proccess(obj) {
-    obj.ret = (!obj.ret) ? 'inter' : obj.ret;
-
-}
-
-
-
-function processa(obj) {
-    if (!obj.ret) {
-        obj.ret = 'inter';
+/**
+ * Funcao de exibição do gif de carregamento
+ * @param {type} visible
+ * @returns {undefined}
+ */
+function showLoader(visible) {
+    if (visible) {
+        $("#loader").fadeIn("fast");
+        lockClick();
+    } else {
+        $("#loader").fadeOut("fast");
+        unlockClick();
     }
+}
 
-    var ajax = initAjax(obj);
-
-
-    ajax.fail(function () {
-        alert("Não foi possível enviar requisição para o servidor: \n" + obj.url);
-    });
+function lockClick(time) {
+    GLOBALSIS.canClick = false;
     
-    ajax.complete(function(){
-        alert("Finalizando");
+    //Se for definido um tempo (time), seta timeout para o tempo definido
+    //Se nao houver, seta o timeout com o default
+    var timeout = (time)?time:GLOBALSIS.timeOutButton;
+    
+    initTimeOut(timeout, function () {
+        unlockClick();
     });
 }
 
-function ajaxControl(obj) {
-    var ajax;
-    
-    if (obj.frm) { //Se houver formulario
-        ajax = $.ajax({
-            method: "POST",
-            url: obj.url,
-            data: getInputsForm(obj.frm) //Retorna JSON com os dados do form
-        });
-    } else { //Se não houver formulario
-        ajax = $.ajax({
-            method: "GET",
-            url: obj.url + "?" + Math.ceil(Math.random() * 100000)
-        });
+function unlockClick() {
+    GLOBALSIS.canClick = true;
+}
+
+/**
+ * 
+ * Pegar uma instancia do ajax
+ * @param obj {url:'url', (opt)frm:'form_func'}
+ * @returns {AJAX}
+ * 
+ */
+function initAjax(obj) {
+    var params = {};
+
+    if (obj.frm) { //Se houver dados para enviar
+        params.data = getInputsForm(obj.frm);
+        params.type = "POST";
+        params.url = GLOBALSIS.path + obj.url;
+    } else {
+        params.type = "GET";
+        params.url = GLOBALSIS.path + obj.url + "?" + Math.ceil(Math.random() * 100000)
     }
     ;
 
-    ajax.fail(function(){
-        alert("Não foi possível encontrar o servidor");
+    return $.ajax(params);
+}
+
+/**
+ * Método de envio/recebimento de dados para o servidor
+ * @param {type} obj
+ * @returns {undefined}
+ */
+function processa(obj) {
+    if (!GLOBALSIS.canClick) {
+        return;
+    }
+
+    showLoader(true);
+
+    //Verifica se há o param ret. Se não há, seta como inter
+    obj.ret = (!obj.ret) ? "inter" : obj.ret;
+
+    var ajax = initAjax(obj);
+
+    //Conexão falhou
+    ajax.fail(function () {
+        alert("Não foi possível enviar requisição para o servidor: \n" + obj.url);
     });
-    
-    ajax.complete(function(){
-        alert("Ajax completo");
-    });
-    
-   
+
+    //Conexão sucedida
     ajax.done(function (data) {
-        if(obj.ret){
+        if (obj.ret) {
             $("#" + obj.ret).html(data);
         }
+    });
+
+    //Completa a requisição se for sucedida ou não
+    ajax.aways(function () {
+        showLoader(false);
     });
 }
 ;
