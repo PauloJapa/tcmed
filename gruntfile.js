fd00fa5d649e971e1dee9ffe4a2054ce40696618
Danilo <danilo.dorotheu@live.com>
2015-07-06 16:05:04 -0300

    commit de modificações

--- gruntfile.js
+++ gruntfile.js
@@ -75,6 +75,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
     // Tarefas que serão executadas
     grunt.registerTask('default', ['uglify', 'sass']);
