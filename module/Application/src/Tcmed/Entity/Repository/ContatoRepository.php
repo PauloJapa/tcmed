3c4d979f0210262eb00248d84db79395d0b3eb43
Allan Davini <allan_mv19@hotmail.com>
2015-08-04 14:19:26 -0300

    Crud Contatos, falta mudar a logica!

---
+++ module/Application/src/Tcmed/Entity/Repository/ContatoRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this license
+ */
+
+namespace Tcmed\Entity\Repository;
+
+/**
+ * Description of CidadeRepository
+ *
+ */
+class CidadeRepository extends \Application\Entity\Repository\AbstractRepository {
+    
+    
+
+}
